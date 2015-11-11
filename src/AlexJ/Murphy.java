e2e81bd88a849d420b77c711dd821a1cc8f5b7a4
mrrobato <astasia_121@hotmail.com>
2015-11-11 20:46:24 +0100

    erstes

---
+++ src/AlexJ/Murphy.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AlexJ;
+import robocode.Robot;
+import robocode.ScannedRobotEvent;
+/**
+ *
+ * @author Ni7mo
+ */
+public class Murphy extends Robot{
+      
+          public void run(){
+        while(true){
+            ahead(100);
+            turnGunRight(360);
+            back(100);
+            turnGunLeft(360);
+            //erstellt1
+        }
+    }
+   public void onScannedRobot(ScannedRobotEvent e){
+     
+   fire(1);
+   }  
+}
